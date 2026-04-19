f4767cf2a80b71b3bc21af4f688158e361f7e0f7
Ethan Herman <ethan34787@gmail.com>
2026-04-18 22:17:02 -0700

    atoms: strip to minimal deps and self-contained fullscreen quad

--- example/quad.h
+++ example/quad.h
@@ -1,10 +1,8 @@
 #pragma once
 
-#include <glm/glm.hpp>
-#include "model.h"
+#include <memory>
+#include <vector>
 #include "wgfx.h"
-
-#include "constants.h"
 
 class Quad
 {
@@ -16,55 +14,43 @@ public:
 	}
 
 	wgfx::Pipeline* pipeline;
+	std::unique_ptr<wgfx::VertexBuffer> vbo_;
+	std::unique_ptr<wgfx::IndexBuffer> ibo_;
 
 	Quad()
 	{
 		pipeline = wgfx::loadPipeline(wgfx::loadFromFile((std::string(RESOURCE_DIR) + "/" + "atoms_sphere.wgsl").c_str()));
 
-		float quadWidth = 1.0f;
-		float quadHeight = 1.0f;
+		const std::vector<float> vertices = {
+			-1.0f, -1.0f, 0.0f,
+			 1.0f, -1.0f, 0.0f,
+			 1.0f,  1.0f, 0.0f,
+			-1.0f,  1.0f, 0.0f
+		};
+		const std::vector<uint16_t> indices = {
+			0, 1, 2,
+			0, 2, 3
+		};
 
-		// Offset from center
-		float x = 0.0f;
-		float y = 0.0f;
+		vbo_.reset(wgfx::createVertexBuffer(vertices));
+		vbo_->setAttribute(0, wgfx::vec3f, 0);
+		ibo_.reset(wgfx::createIndexBuffer(indices));
 
-		quad_.addData({
-			//   x,             y,            z
-			-quadWidth + x, -quadHeight + y, 0.0f,  // bottom-left
-			 quadWidth + x, -quadHeight + y, 0.0f,  // bottom-right
-			 quadWidth + x,  quadHeight + y, 0.0f,  // top-right
-			-quadWidth + x,  quadHeight + y, 0.0f   // top-left
-			}, {
-				0, 1, 2,
-				0, 2, 3
-			});
-
-		wgfx::VertexBuffer* vbo = wgfx::createVertexBuffer();
-		vbo->setAttribute(0, wgfx::vec3f, 0); // pos
-
-		std::vector<float> verticies;
-		verticies.push_back(0.0);
-
-		std::vector<uint16_t> indicies;
-		indicies.push_back(0);
-
-		pipeline->setVertexBuffer(verticies);
-		pipeline->setIndexBuffer(indicies);
+		pipeline->setVertexBuffer(vbo_.get());
+		pipeline->setIndexBuffer(ibo_.get());
 
 		pipeline->targets = 1;
 		pipeline->useDepth = false;
 
-		pipeline->init(vbo);
+		pipeline->init(vbo_.get());
 	}
 
 	void render()
 	{
 		pipeline->uniforms.clear(); // Reset uniform count
-		quad_.bind(pipeline);
+		pipeline->setVertexBuffer(vbo_.get());
+		pipeline->setIndexBuffer(ibo_.get());
 	}
-
-private:
-	Model quad_;
 
 public:
 	Quad(Quad const&) = delete;
